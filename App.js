6fc5d27d5bab72b15b96107d3b16c4c3ac9491a9
Randhir <kumarrandhir111@gmail.com>
2025-04-12 19:53:46 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *      <div id="child">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ * </div>
+ * 
+ *  ReactElement(object) => HTML(Browser Understand)
+ */
+
+
+const parent = React.createElement("div", { id: "parent"}, [
+    React.createElement("div", { id: "child"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2"}, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+]);
+
+
+console.log(parent); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
